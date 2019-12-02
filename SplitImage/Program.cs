316ba5abe7d624f98163be5c711251788406fb90
Felipe Marques <felipe.msfg@gmail.com>
2019-12-01 21:34:41 -0300

    divisor de imagens no meio

---
+++ SplitImage/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace SplitImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadPngFromFolder(@"D:\Google Drive\Mestrado\52 - Base de imagens\working_directory", @"D:\Google Drive\Mestrado\52 - Base de imagens\destination_path");
+        }
+
+        private static void ReadPngFromFolder(string path, string destinationPath)
+        {
+            foreach (string file in Directory.EnumerateFiles(path, "*.png"))
+            {
+                SplitImages(file, destinationPath);
+            }
+        }
+
+        private static void SplitImages(string imagePath, string destinationPath)
+        {
+            // Get the inputs.
+            var bm = Image.FromFile(imagePath);
+            var width = (int) (bm.Width / 2);
+            var height = bm.Height;
+
+            var pieceName = Path.GetFileNameWithoutExtension(imagePath);
+            var piece = new Bitmap(width, height);
+
+            // Left
+            var destRect = new Rectangle(0, 0, width, height);
+            using var gr = Graphics.FromImage(piece);
+            var sourceRect = new Rectangle(0, 0, width, height);
+            gr.DrawImage(bm, destRect, sourceRect, GraphicsUnit.Pixel);
+
+            // Save the piece.
+            var filename = $@"{destinationPath}\{pieceName} esquedo.png";
+            piece.Save(filename, ImageFormat.Png);
+
+            // Right
+            using var grRight = Graphics.FromImage(piece);
+            var sourceRightRect = new Rectangle(bm.Width - width, 0, width, height);
+            gr.DrawImage(bm, destRect, sourceRect, GraphicsUnit.Pixel);
+
+            // Save the piece.
+            filename = $@"{destinationPath}\{pieceName} direito.png";
+            piece.Save(filename, ImageFormat.Png);
+        }
+    }
+}
---
+++ SplitImageFramework/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitImageFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadPngFromFolder(@"D:\Google Drive\Mestrado\52 - Base de imagens\working_directory", @"D:\Google Drive\Mestrado\52 - Base de imagens\destination_path");
+        }
+
+        private static void ReadPngFromFolder(string path, string destinationPath)
+        {
+            foreach (string file in Directory.EnumerateFiles(path, "*.png"))
+            {
+                SplitImages(file, destinationPath);
+            }
+        }
+
+        private static void SplitImages(string imagePath, string destinationPath)
+        {
+            // Get the inputs.
+            var bm = Image.FromFile(imagePath);
+            var width = (int)(bm.Width / 2);
+            var height = bm.Height;
+
+            var pieceName = Path.GetFileNameWithoutExtension(imagePath);
+            var piece = new Bitmap(width, height);
+
+            // Left
+            var destRect = new Rectangle(0, 0, width, height);
+            using (var gr = Graphics.FromImage(piece))
+            {
+                var sourceRect = new Rectangle(0, 0, width, height);
+                gr.DrawImage(bm, destRect, sourceRect, GraphicsUnit.Pixel);
+
+                // Save the piece.
+                var filename = $@"{destinationPath}\{pieceName} esquerdo.png";
+                piece.Save(filename, ImageFormat.Png);
+            }
+
+
+            using (var gr = Graphics.FromImage(piece))
+            {
+                // Right
+                var sourceRightRect = new Rectangle(bm.Width - width, 0, width, height);
+                gr.DrawImage(bm, destRect, sourceRightRect, GraphicsUnit.Pixel);
+
+                // Save the piece.
+                var filename = $@"{destinationPath}\{pieceName} direito.png";
+                piece.Save(filename, ImageFormat.Png);
+            }
+        }
+    }
+}
